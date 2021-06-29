a9763d271b0e0caf245a1cf5862e668baadd8763
Diego Palat <95026780@lidl.de>
2021-06-30 00:28:13 +0200

    refactor

---
+++ MiddleEarthObject.cs
@@ -0,0 +1,9 @@
+namespace LordOfTheRing.Functions
+{
+    public class MiddleEarthObject
+    {
+        public string id { get; set; }
+        public string type { get; set; }
+        public string subtype { get; set; }
+    }
+}
--- RingFinderFunction.cs
+++ RingFinderFunction.cs
@@ -4,7 +4,8 @@ using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Host;
 using Microsoft.Extensions.Logging;
 using System.Threading.Tasks;
-namespace Pipin.Function
+
+namespace LordOfTheRing.Functions
 {
     public static class RingFinderFunction
     {
@@ -13,31 +14,31 @@ namespace Pipin.Function
         public static List<string> _listOfUniqueRings = new List<string>();
 
         [FunctionName(TeamNameFunctionName)]
-        public static async Task RunFinderFunction(
-            [ServiceBusTrigger("cracks-of-doom", TeamName, Connection = "katasbdevwe001_serviceBus")] 
-                MiddleearthObject middleearthObject,
+        public static async Task RunAsync(
+            [ServiceBusTrigger("cracks-of-doom", TeamName, Connection = "ServiceBusConnection")]
+                MiddleEarthObject middleearthObject,
             [CosmosDB(
                     databaseName: "Middle-Earth",
                     collectionName: "Mordor",
-                    CreateIfNotExists = true, 
+                    CreateIfNotExists = true,
                     PartitionKey = "/_pk",
-                    ConnectionStringSetting = "CosmoDBConnection")] 
+                    ConnectionStringSetting = "CosmoDBConnection")]
                 IAsyncCollector<dynamic> cosmosDbDocuments,
                 ILogger log)
         {
             if (middleearthObject.type.ToLower() != "ring") return;
 
-            log.LogInformation($"We found a [Ring] with sub-type '{middleearthObject.subtype}', id: '{middleearthObject.id}'"); 
+            log.LogInformation($"We found a [Ring] with sub-type '{middleearthObject.subtype}', id: '{middleearthObject.id}'");
 
             if (middleearthObject.subtype.ToLower() != "uniquering") return;
-            
+
             log.LogInformation($"We found a [Unique Ring]!!, id: '{middleearthObject.id}'");
 
-            if(_listOfUniqueRings.Exists(id => id == middleearthObject.id))
+            if (_listOfUniqueRings.Exists(id => id == middleearthObject.id))
             {
                 log.LogWarning($"We found all the [Unique Rings]!! total unique rings: '{_listOfUniqueRings.Count}'.");
-                var document = new 
-                { 
+                var document = new
+                {
                     Description = $"We found '{_listOfUniqueRings.Count}' a unique Rings, we are '{TeamName}' team",
                     team = TeamName,
                     _pk = TeamName,
@@ -46,18 +47,12 @@ namespace Pipin.Function
                 await cosmosDbDocuments.AddAsync(document);
 
                 _listOfUniqueRings = new List<string>();
-            } 
-            else 
+            }
+            else
             {
                 _listOfUniqueRings.Add(middleearthObject.id);
             }
         }
     }
 
-    public class MiddleearthObject 
-    {
-        public string id { get; set; }
-        public string type { get; set; }
-        public string subtype { get; set; }
-    }
 }
